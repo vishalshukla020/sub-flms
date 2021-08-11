fef7d23cd3777d4b6fc57036df52fa557734a424
vishal shukla <shuklavishal737@gmail.com>
2021-08-11 16:04:44 +0530

    changed base url

--- helper/baseURL.js
+++ helper/baseURL.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://law-production-2021.vercel.app"
+    ? "https://flmsservices-2021.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
